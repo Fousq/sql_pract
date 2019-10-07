07a0c94b2f03d288c3dc0e1c54f86f0a3be4eab4
Zhanbolat Orakbayev <zhanbolat_orakbayev@epam.com>
2019-10-07 18:06:34 +0600

    Make 2 script into 1, 3 not yet binded and cann't work

--- script.sql
+++ script.sql
@@ -1,22 +1,65 @@
 --calculate the number of students of each course;
-select course.code, count(*) AS "Number of student" from student
+select course.code, count(student.id) AS "Number of student" from student
 inner join student_course on student.id = student_course.student_id 
-inner join course on course.code = student_course.course_code 
+right join course on course.code = student_course.course_code 
 group by course.code;
+
+select * from course
+left join student_course on student_course.course_code = course.code;
+
+create view course_lecturers_amount as
+select course.code, count(lecturer_course.lecturer_id) as lecturers_amount from lecturer_course
+right join course on course.code = lecturer_course.course_code
+group by course.code;
+
+create view course_students_num as
+select course.code, count(student_course.student_id) as students_num
+from course
+left join student_course on student_course.course_code = course.code
+group by course.code;
+
+select course_students_num.code, course_students_num.students_num, 
+course_lecturers_amount.lecturers_amount from course_students_num
+inner join course_lecturers_amount on course_students_num.code = course_lecturers_amount.code;
+
+with ctp(code) as
+(select * from student
+full join student_course on student.id = student_course.student_id
+full join course on course.code = student_course.course_code
+full join lecturer_course on lecturer_course.course_code = student_course.course_code
+union all
+select course.code, count(lecturer_course.lecturer_id) as "lecturers amount" from lecturer_course
+right join course on lecturer_course.course_code = course.code
+group by course.code
+)
+ select * from ctp;
 
 --amount of lecturers of this course;
 select count(*) from lecturer_course where lecturer_course.course_code="[couser's code]";
 
 --name of the lecturer with the higest academic degree on this course
-select lecturer.name from lecturer
+select lecturer_course.course_code as code, lecturer.name from lecturer
 inner join academic_degree on academic_degree.id = lecturer.academic_degree
 inner join lecturer_course on lecturer.id = lecturer_course.lecturer_id
-where academic_degree.priority = (select max(academic_degree.priority) from academic_degree
+inner join (select lecturer_course.course_code,
+ max(academic_degree.priority) as academic_degree from academic_degree
 inner join lecturer on lecturer.academic_degree = academic_degree.id
 inner join lecturer_course on lecturer.id = lecturer_course.lecturer_id
-and lecturer_course.course_code = "[couser's code]")
-and lecturer_course.course_code = "[couser's code]"
-limit 1;
+group by lecturer_course.course_code);
+
+create view max_ad as
+ select lecturer_course.course_code,
+  max(academic_degree.priority) as academic_degree,
+  lecturer_course.lecturer_id from academic_degree
+ inner join lecturer on lecturer.academic_degree = academic_degree.id
+ inner join lecturer_course on lecturer.id = lecturer_course.lecturer_id
+ group by lecturer_course.course_code, lecturer_course.lecturer_id;
+
+ select * from lecturer
+ inner join lecturer_course on lecturer.id = lecturer_course.lecturer_id
+inner join academic_degree on academic_degree.id = lecturer.academic_degree
+inner join max_ad on lecturer.id = max_ad.lecturer_id
+group by lecturer.name, lecturer_course.course_code, academic_degree.priority;
 
 --name of the semester in which this course was selected more often
 select semester.name from semester
@@ -38,3 +81,4 @@ and (lecturer_course.course_code = 'M1'
 or student_course.course_code = 'M1');
  
 --https://www.postgresql.org/docs/9.1/tutorial-window.html
+--WITH (subquery) AS query_name
